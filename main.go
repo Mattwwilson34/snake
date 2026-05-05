af4dffbe2a62f5a2fbca5f63aae8b162ad417f16
Matt Wilson <49503056+Mattwwilson34@users.noreply.github.com>
2026-05-04 21:28:36 -0400

    feat: add board refresh logic

--- main.go
+++ main.go
@@ -1,19 +1,44 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"time"
+)
 
 func main() {
 	fmt.Println("Welcome to SNAKE")
 
+	gameover := false
 	board := [10][10]int{}
+	snake := 4
+	snakeX := 1
+	snakeY := 1
+
 	render(&board)
+
+	// Main game loop
+	for {
+		if gameover {
+			print("GAME OVER")
+			break
+		}
+		fmt.Print("\033[10A")
+		board[snakeY][snakeX] = snake
+		render(&board)
+		time.Sleep(200 * time.Millisecond)
+		if snakeX >= len(board)-1 {
+			break
+		}
+		board[snakeY][snakeX] = 0
+		snakeX++
+	}
 }
 
 // main render function
 func render(board *[10][10]int) {
 	for _, row := range board {
 		for _, val := range row {
-			fmt.Printf("%2d ", val)
+			fmt.Printf("%2d", val)
 		}
 		fmt.Println()
 	}
