6f6dd6afb2be1dd752236c5737de214de7885f4a
Matt Wilson <49503056+Mattwwilson34@users.noreply.github.com>
2026-05-04 21:28:31 -0400

    feat: print board to screen

--- main.go
+++ main.go
@@ -3,5 +3,18 @@ package main
 import "fmt"
 
 func main() {
-	fmt.Println("hello from snake")
+	fmt.Println("Welcome to SNAKE")
+
+	board := [10][10]int{}
+	render(&board)
+}
+
+// main render function
+func render(board *[10][10]int) {
+	for _, row := range board {
+		for _, val := range row {
+			fmt.Printf("%2d ", val)
+		}
+		fmt.Println()
+	}
 }
