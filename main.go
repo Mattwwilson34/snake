2636357540dea7b93d77671963e5921cff702bd9
Matt Wilson <49503056+Mattwwilson34@users.noreply.github.com>
2026-05-05 20:27:44 -0400

    feat: add board struct and methods

--- main.go
+++ main.go
@@ -5,6 +5,10 @@ import (
 	"time"
 )
 
+type Board struct {
+	grid [10][10]rune
+}
+
 type Position struct {
 	X, Y int
 }
@@ -16,22 +20,14 @@ type Snake struct {
 }
 
 func main() {
-	fmt.Println("Welcome to SNAKE")
-
 	gameover := false
 
-	board := [10][10]rune{}
-	dot := '\u00B7'
-	for i := range 10 {
-		for j := range 10 {
-			board[j][i] = dot
-		}
-	}
 	snake := Snake{Symbol: '★',
 		Position: Position{X: 0, Y: 0},
 	}
 
-	render(&board)
+	board := NewBoard()
+	Render(board)
 
 	// Main game loop
 	for {
@@ -40,23 +36,33 @@ func main() {
 			break
 		}
 		fmt.Print("\033[10A")
-		board[snake.Y][snake.X] = snake.Symbol
-		render(&board)
+		board.grid[snake.Y][snake.X] = snake.Symbol
+		Render(board)
 		time.Sleep(200 * time.Millisecond)
-		if snake.X >= len(board)-1 {
+		if snake.X >= len(board.grid)-1 {
 			break
 		}
-		board[snake.Y][snake.X] = dot
+		board.grid[snake.Y][snake.X] = '·'
 		snake.X++
 	}
 }
 
 // main render function
-func render(board *[10][10]rune) {
-	for _, row := range board {
+func Render(b *Board) {
+	for _, row := range b.grid {
 		for _, val := range row {
 			fmt.Printf("%2c", val)
 		}
 		fmt.Println()
 	}
+}
+
+func NewBoard() *Board {
+	b := &Board{}
+	for i := range 10 {
+		for j := range 10 {
+			b.grid[j][i] = '·'
+		}
+	}
+	return b
 }
