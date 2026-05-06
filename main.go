e7512a731c47d64d1a67b61ccafd48687a174372
Matt Wilson <49503056+Mattwwilson34@users.noreply.github.com>
2026-05-05 20:07:50 -0400

    feat: update grid to use runes with dots for cleaner UI

--- main.go
+++ main.go
@@ -5,14 +5,31 @@ import (
 	"time"
 )
 
+type Position struct {
+	X, Y int
+}
+
+// For now only position will need to sort out how to add length
+type Snake struct {
+	Position
+	Symbol rune
+}
+
 func main() {
 	fmt.Println("Welcome to SNAKE")
 
 	gameover := false
-	board := [10][10]int{}
-	snake := 4
-	snakeX := 1
-	snakeY := 1
+
+	board := [10][10]rune{}
+	dot := '\u00B7'
+	for i := range 10 {
+		for j := range 10 {
+			board[j][i] = dot
+		}
+	}
+	snake := Snake{Symbol: '★',
+		Position: Position{X: 0, Y: 0},
+	}
 
 	render(&board)
 
@@ -23,22 +40,22 @@ func main() {
 			break
 		}
 		fmt.Print("\033[10A")
-		board[snakeY][snakeX] = snake
+		board[snake.Y][snake.X] = snake.Symbol
 		render(&board)
 		time.Sleep(200 * time.Millisecond)
-		if snakeX >= len(board)-1 {
+		if snake.X >= len(board)-1 {
 			break
 		}
-		board[snakeY][snakeX] = 0
-		snakeX++
+		board[snake.Y][snake.X] = dot
+		snake.X++
 	}
 }
 
 // main render function
-func render(board *[10][10]int) {
+func render(board *[10][10]rune) {
 	for _, row := range board {
 		for _, val := range row {
-			fmt.Printf("%2d", val)
+			fmt.Printf("%2c", val)
 		}
 		fmt.Println()
 	}
